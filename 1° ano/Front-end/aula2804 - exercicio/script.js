41a27f0289832e199723a4785f331ad8e975e980
Junior Silva <150052476+juniorlds98@users.noreply.github.com>
2025-05-12 20:47:30 -0300

    Add files via upload

--- 1° ano/Front-end/aula2804 - exercicio/script.js
+++ 1° ano/Front-end/aula2804 - exercicio/script.js
@@ -1,3 +1,22 @@
+let filter = document.querySelectorAll(".time_filter-item")
+let jogador = document.querySelectorAll(".time_jogador")
+
+filter.forEach(elem => {
+    elem.addEventListener("click", () =>{
+        filterId = elem.getAttribute("id")
+
+        jogador.forEach(idx => {
+            posicao = idx.getAttribute("class")
+
+            if(filterId == "all"){
+                idx.classList.remove("disable")
+            } else if(posicao != ("time_jogador " + filterId)){
+                idx.classList.add("disab")
+            }
+        })
+    })
+})
+
 const nome = document.querySelector("#nome")
 const numero = document.querySelector("#numero")
 const btn = document.querySelector("#btn")
