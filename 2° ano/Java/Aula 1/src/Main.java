4bbc6bdefde6fbff326fe8a718f97084f0e2a2e7
Junior Silva <150052476+juniorlds98@users.noreply.github.com>
2025-08-05 21:20:43 -0300

    Add files via upload

---
+++ 2° ano/Java/Aula 1/src/Main.java
@@ -0,0 +1,22 @@
+
+public class Main {
+    public static void main(String[] args) {
+        //Criando uma variável
+        var nome = "Junior";
+
+        //Fazendo output do dado - print
+
+        System.out.println("Olá mundo, preciso de sorte " + nome);
+
+        //sout é um atalho para print
+
+        //Print sem o ln não pula a linha
+        var sobrenome = "Silva";
+        System.out.print(nome);
+        System.out.print(sobrenome);
+
+        //Print sem o ln pula a linha
+        System.out.println(nome);
+        System.out.println(sobrenome);
+    }
+}
