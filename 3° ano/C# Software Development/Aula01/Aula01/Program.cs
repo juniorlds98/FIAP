5ab702c1fb52df6496d3399820e36cd7d11ca092
Junior Silva <150052476+juniorlds98@users.noreply.github.com>
2026-08-03 20:47:15 -0300

    Add files via upload

---
+++ 3° ano/C# Software Development/Aula01/Aula01/Program.cs
@@ -0,0 +1,21 @@
+﻿int anosAutonomia = 450; //Int
+double capacidadeBateria = 75.5; //Float
+string modeloCarro = "Brasilia";
+bool carregamentoRapido = true;
+
+Console.WriteLine($" Qual a autonomia do Veiculo {anosAutonomia}");
+Console.WriteLine($" Capacidade da bateria: {capacidadeBateria}");
+Console.WriteLine($" Modelo do carro: {modeloCarro}");
+Console.WriteLine($" Velocidade de carregamento: {carregamentoRapido}");
+
+
+Console.Write("Digite a voltagem do carregador (110V / 220V)"); //Comentario
+string entrada = Console.ReadLine(); //Input
+
+string modelo = "      byd      ";
+
+string formatado = modelo.ToUpper().Trim(); //Métodos
+
+Console.WriteLine(formatado);
+
+
